cf6ad3873269b3aa01fe95d498bd4c4ffec980b1
Ayyapa2005 <tkdsoulaanand2005@gmail.com>
2025-03-13 13:55:09 +0530

    multi array

--- mularray/marray.java
+++ mularray/marray.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
 public class  marray{
     public static void main(String args[]){
         int arr[][]={{2,3,4,5,6,7},{7,8,5,3,4,9},{1,8,6,3,9,0}};
-        System.out.println(arr);
+        
+        System.out.println(Arrays.deepToString(arr));
         System.out.println(arr[0][1]);
     }
 }
