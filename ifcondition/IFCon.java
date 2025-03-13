b1c66e85968486d9bcc46baa7c943b4df3ac31ac
Ayyapa2005 <tkdsoulaanand2005@gmail.com>
2025-03-13 10:54:09 +0530

    if else

---
+++ ifcondition/IFCon.java
@@ -0,0 +1,11 @@
+public class  IFCon{
+    public static void main(String args[]){
+        int no = 5;
+        if(no%2==0){
+            System.out.println("The number is Even");
+        }
+        else{
+            System.out.println("The numbr is odd");
+        }
+    }
+}
