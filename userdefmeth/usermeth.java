76b52d0b379222f0f60424f8d88856cee02ae56d
Ayyapa2005 <tkdsoulaanand2005@gmail.com>
2025-03-14 13:34:33 +0530

    user def method

--- userdefmeth/usermeth.java
+++ userdefmeth/usermeth.java
@@ -1,15 +1,33 @@
-public class usermeth{
-    void add(int a,int b){
-        System.out.println("sum is "+(a+b));
-
+public class cal{
+    public static int  sum(int a,int b){
+         int sum = a+b;
+         return sum;
     }
-    void add(int a,int b,int c){
-        System.out.println("sum is "+(a+b+c));
-        
+    public static int sum(int a, int b,int c){
+        int sum = a+b+c;
+        return sum;
     }
-        public static void main(String args[]){
-        usermeth obj=new usermeth();
-        obj.add(10,20);
-        obj.add(20,30,40);
-    }
-    }
+    
+ 
+     
+    public static int subtract(int a, int b){
+         int sub = a-b;
+         return sub;
+     }
+    public static int divide(int a, int b){
+         int div = a/b;
+         return div;
+     }
+ }
+ public class usermeth{
+    public static void main(String args[]){
+         int sumre=cal.sum(2,3);
+         int subre = cal.subtract(5,3);
+         int divre = cal.divide(10,2);
+         int su = cal.sum(3,4,6);
+         System.out.println("sum of two number is "+sumre);
+         System.out.println("subtraction of two number is "+subre);
+         System.out.println("division of two number is "+divre);
+         System.out.println("sum is "+su);
+     }
+ }
