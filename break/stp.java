364b09f0f7a1c6e5acde8431deed72bc23303e49
Ayyapa2005 <tkdsoulaanand2005@gmail.com>
2025-03-14 14:35:41 +0530

    break

---
+++ break/stp.java
@@ -0,0 +1,12 @@
+public class stp{
+    public static void main(String args[]){
+        int i = 0;
+        while(true){
+            i=i+1;
+            if(i==2){
+                System.out.println("stopped");
+            }
+            break;    
+        }
+    }
+}
