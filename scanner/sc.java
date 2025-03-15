e1034bed7eca6ff4f95688f328eedeed61583662
Ayyapa2005 <tkdsoulaanand2005@gmail.com>
2025-03-15 09:43:52 +0530

    scanner

---
+++ scanner/sc.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class  sc{
+    public static void main(String args[]){
+        Scanner inn = new Scanner(System.in);
+        System.out.println("ente your name: ");
+        String name = inn.nextLine();
+    }
+}
