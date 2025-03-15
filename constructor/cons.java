06501919e2ce3cd9a796e43a936bb114a6ae4044
Ayyapa2005 <tkdsoulaanand2005@gmail.com>
2025-03-15 10:52:03 +0530

    constructor

---
+++ constructor/cons.java
@@ -0,0 +1,15 @@
+class  construc{
+    construc(){
+        System.out.println("python");
+}
+    void disp(){
+        System.out.println("hi");
+    }
+}
+public class cons {
+    public static void main(String args[]){
+        construc re = new construc();
+        re.disp();
+
+    }
+}
